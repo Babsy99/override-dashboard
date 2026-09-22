94811bf9647498fd0c4d6af0884d4f6b408b3e26
Babsy99 <164735629+Babsy99@users.noreply.github.com>
2026-09-22 05:03:58 -0400

    Load packed fingerprints then decode

--- sprite-fp.js
+++ sprite-fp.js
@@ -1,12 +1,22 @@
 window.SPRITE_FP_B64=window.SPRITE_FP_B64||{};
 (function(){
-  var src=window.SPRITE_FP_B64||{};
-  var db={};
-  function dec(s){
-    var bin=atob(s),n=bin.length,vec=new Float32Array(n);
-    for(var i=0;i<n;i++) vec[i]=bin.charCodeAt(i)/255;
-    return vec;
+  var files=['Jonesy','Bush','Adventure','8-Bit','Sonic','Tails','Shadow','Killswitch','Jackrabbit','Klombo','Crown','Storm-Scout'];
+  function decode(){
+    var src=window.SPRITE_FP_B64||{}, db={};
+    function dec(s){
+      var bin=atob(s), n=bin.length, vec=new Float32Array(n);
+      for(var i=0;i<n;i++) vec[i]=bin.charCodeAt(i)/255;
+      return vec;
+    }
+    Object.keys(src).forEach(function(k){ db[k]={fp:dec(src[k])}; });
+    window.SPRITE_FP=db;
   }
-  Object.keys(src).forEach(function(k){ db[k]={fp:dec(src[k])}; });
-  window.SPRITE_FP=db;
+  var left=files.length;
+  if(!left){decode();return;}
+  files.forEach(function(name){
+    var s=document.createElement('script');
+    s.src='fp/'+name+'.js';
+    s.onload=s.onerror=function(){ if(--left<=0) decode(); };
+    document.head.appendChild(s);
+  });
 })();
